1a0590c3d00748125915ba55e7d9f1f6a91659b7
Santhosh Patel <santhoshpatel002@gmail.com>
2026-07-24 22:09:49 +0530

    refactor: replace App component with MarketingApp and update routing in main.jsx for improved navigation structure

--- landing-page/src/main.jsx
+++ landing-page/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import MarketingApp from './MarketingApp.jsx';
+import DocsLayout from './docs/DocsLayout.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MarketingApp />} />
+        <Route path="/docs/*" element={<DocsLayout />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
